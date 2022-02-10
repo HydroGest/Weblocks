3090b52ee35153588eaf6d44b397e5c2698b5f99
HydroGest <2445691453@qq.com>
2022-02-10 17:41:37 +0800

    Update sites load

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+console.log("Reading librarys...");
 const express = require('express');
 const app = express();
-const port = 8073;
+console.log("Reading config...");
+const config = require('./config.json');
+const port = config.port;
+const siteList = require(config.siteIndex);
+console.log("Loading sites list...");
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('<center><h1>Weblocks</h1><HR/><b>Hello World!</b><br/>Another weblocks server.</center>')
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Your app '`+config.appName+`' listening on port ${port}`);
 });
