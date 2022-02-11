f0e810e772a227171d88b3e8172572766c3cd9ef
HydroGest <2445691453@qq.com>
2022-02-11 08:03:35 +0800

    Fix package.json

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 console.log("Reading librarys...");
+const readline=require('readline');
 const express = require('express');
 const app = express();
+const rl=readline.createInterface({
+	input:process.stdin,
+	output:process.stdout
+})
 console.log("Reading config...");
 const config = require('./config.json');
 const port = config.port;
+
 //const siteList = require(config.siteIndex);
 
 app.get('/', (req, res) => {
@@ -11,5 +17,14 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Your app '`+config.appName+`' listening on port ${port}`);
+    console.log(`Your app '`+config.appName+`' listening on port ${port}`);
+    console.log('Type`stop` to stop.');
+    while(1){
+        rl.question('> ',function(ans){
+            if(ans=='stop'){
+                process.exit(0);
+            }
+            rl.close();
+        });
+    }
 });
